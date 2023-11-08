21678909203dcec7654ac6bdd60e58888d007646
손수민 <ssmstar@sungkyul.ac.kr>
2023-11-08 05:13:26 +0000

    11주차 실습내용

--- WEB-INF/src/dto/Product.java
+++ WEB-INF/src/dto/Product.java
@@ -14,6 +14,7 @@ public class Product implements Serializable { // 인터페이스 선언
 	private long unitsInStock; //재고수 
 	private String condition; 	//신상품 or 중고품 or 재생품
     private String filename; 	// 파일 이름 변수
+    private int quantity;	//장바구니에 담은 개수
 
     
     public Product() {
@@ -92,6 +93,14 @@ public class Product implements Serializable { // 인터페이스 선언
     
     public void setFilename(String filename) { // 파일 이름 설정
 	    this.filename = filename;
+    }
+    
+    public int getQuantity() {
+	    return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+	    this.quantity = quantity;
     }
 
     public String getFilename() { // 파일 이름 리턴
